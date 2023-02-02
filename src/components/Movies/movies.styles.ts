0396ed26ff4735b32df1472d122caa671ce3e015
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-02 15:00:43 -0300

    feat: add Movies Styles

---
+++ src/components/Movies/movies.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { motion } from 'framer-motion'
+
+export const Movies = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-column-gap: 1rem;
+  grid-row-gap: 2rem;
+  font-size: 10px;
+`
