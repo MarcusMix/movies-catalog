184474b192fd68199e35c000c8f36249f278f404
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-02 15:00:19 -0300

    feat: add Movies Component

---
+++ src/components/Movies/movies.component.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react"
+import { motion } from 'framer-motion'
+
+interface MovieProps {
+    movie: any
+}
+
+const Movie:FC<MovieProps>  = ({movie})=> {
+    return (
+        <motion.div 
+          layout
+          animate={{opacity: 1}} 
+          initial={{opacity: 0}} 
+          exit={{opacity: 0}}
+        > 
+            <h2>{movie.title}</h2>
+            <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} alt="" />
+        </motion.div>
+    )
+}
+
+export default Movie
