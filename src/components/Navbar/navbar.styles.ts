ac0c38f18e651a83eda134e32813f0dd96c93959
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-02 23:04:03 -0300

    feat: add Navbar Styles

---
+++ src/components/Navbar/navbar.styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const NavbarContainer = styled.nav `
+    width: 100%;
+    height: 80px;
+    background-color: #051923;
+`
+
+export const NavbarContent = styled.div `
+    color: white;
+    padding: 10px;
+    text-align: center;
+`
+
+export const TitleWhite = styled.h1 `
+    color: white;
+`
+
+export const TitleYellow = styled.h1 `
+    color: #FFC914;
+`
