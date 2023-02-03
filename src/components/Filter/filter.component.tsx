e4244863cc38fdc8a96a9f0681687393486dd971
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-03 13:49:31 -0300

    feat: add button for new page and icons

--- src/components/Filter/filter.component.tsx
+++ src/components/Filter/filter.component.tsx
@@ -1,9 +1,19 @@
-import { Dispatch, SetStateAction, useEffect } from 'react'
-import { FC } from "react"
-import { ButtonClick } from '../Button/button.styles'
+//react
+import { Dispatch, SetStateAction, useContext, useEffect, FC } from 'react'
 
+//context
+import { MovieContext } from '../../store/movie'
+
+//components
+import { ButtonClick, ButtonPages } from '../Button/button.styles'
+
+//styles
 import { FilterContainer } from './filter.styles'
 
+//icons
+import { HiArrowCircleLeft, HiArrowCircleRight } from 'react-icons/hi'
+
+//interface
 interface FilterProps {
     movies: any
     setFiltered: Dispatch<SetStateAction<string[]>>
@@ -13,7 +23,9 @@ interface FilterProps {
 
 const Filter:FC<FilterProps> = ({movies, setFiltered, setActiveGenre, activeGenre}) => {
 
-    // toda vez q o clicamos no botão, esse useEffect se ativará
+    //context
+    const {moreMovies, setMoreMovies} = useContext(MovieContext)
+
     useEffect(() => {
         if(activeGenre === 0) {
             setFiltered(movies)
@@ -25,14 +37,27 @@ const Filter:FC<FilterProps> = ({movies, setFiltered, setActiveGenre, activeGenr
         setFiltered(filtered)
     },[activeGenre])
 
+
+    const handleMoreMovies = () => {
+        setMoreMovies(moreMovies + 1)
+    }
+
+    const handleBackMovies = () => {
+        if(moreMovies === 1) {
+            return setMoreMovies(moreMovies)
+        }
+        setMoreMovies(moreMovies - 1)
+    }
+
+
     return (
         <FilterContainer>
+            <div>
             <ButtonClick 
                 draggable={activeGenre === 0 ? true : false}
                 onClick={() => setActiveGenre(0)}>
                 Todos
             </ButtonClick>
-           
 
             <ButtonClick 
                 draggable={activeGenre === 35 ? true : false}
@@ -45,6 +70,25 @@ const Filter:FC<FilterProps> = ({movies, setFiltered, setActiveGenre, activeGenr
                 onClick={() => setActiveGenre(28)}
                 >Ação
             </ButtonClick>
+
+            </div>
+
+            <div>
+                <ButtonPages 
+                    onClick={handleBackMovies}
+                    >
+                       <HiArrowCircleLeft size="20"/>  Anterior
+                        
+                </ButtonPages>
+                    {/* <p>Página atual {moreMovies}</p> */}
+                <ButtonPages 
+                    onClick={handleMoreMovies}
+                    >
+                       <HiArrowCircleRight size="20"/> Próximo
+                        
+                </ButtonPages>
+            </div>
+
         </FilterContainer>
     )
 }
