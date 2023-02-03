443fd8da19d7e274ca04425bd3a5b548258e759b
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-03 13:49:53 -0300

    feat: add new ButtonPages and new dimensions

--- src/components/Button/button.styles.ts
+++ src/components/Button/button.styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ButtonClick = styled.button` 
   margin: 10px 10px;
-  height: 1.4rem;
+  height: 1.8rem;
   border: none;
   background-color: ${props => props.draggable ? '#FFC914' : 'white'};
   color: black;
@@ -11,6 +11,15 @@ export const ButtonClick = styled.button`
   font-weight: bold;
   cursor: pointer;
   width: 70px;
-  /* padding-left: 20%; */
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`
+
+export const ButtonPages = styled(ButtonClick) `
+  height: 50px;
+  text-align: center;
+
+  :active {
+    background-color: #FFC914;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+  }
 `
