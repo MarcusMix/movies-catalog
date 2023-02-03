72314df91379803fe50572ddc1b56ecbc15c7f0d
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-03 13:49:03 -0300

    refactor: add media query for responsiveness

--- src/components/Movies/movies.styles.ts
+++ src/components/Movies/movies.styles.ts
@@ -6,6 +6,18 @@ export const Movies = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   grid-column-gap: 1rem;
-  grid-row-gap: 2rem;
-  font-size: 10px;
+  grid-row-gap: 1rem;
+  font-size: 8px;
+
+  @media (max-width: 768px) {
+    display: flex;
+    align-items: center;
+    align-content: center;
+    flex-wrap: wrap;
+    max-width: 250px; 
+    margin: 0 auto;
+    padding: 0;
+    text-align: center;
+    font-size: 10px;
+  }
 `
