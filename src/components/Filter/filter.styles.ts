d0f45d83dc431a5cf2c8ea9953ecf9fd903af07f
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-02 23:04:43 -0300

    refactor: remove button filtered

--- src/components/Filter/filter.styles.ts
+++ src/components/Filter/filter.styles.ts
@@ -2,25 +2,7 @@ import styled from "styled-components";
 
 export const FilterContainer = styled.div `
     color: yellow;
-`
-
-export const ButtonFiltered = styled.button `
-  margin-right: 1rem;
-  height: 2rem;
-  padding: 0.5rem;
-  border: none;
-  background: white;
-  color: #335C67;
-  border-radius: 1rem;
-  border: 2px solid #2B3D41;
-  font-weight: bold;
-  cursor: pointer;
-
-  .active {
-    background-color: #335C67;
-    color: white;
-  }
-  
-`
+    margin-left: 20%;
+    `
 
 
