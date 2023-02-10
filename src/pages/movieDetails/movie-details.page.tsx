b2d6289dc5fce91e3918e789be93645df3bda76d
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-10 14:50:23 -0300

    feat: add movie details page and render component

---
+++ src/pages/movieDetails/movie-details.page.tsx
@@ -0,0 +1,9 @@
+import MovieSpecs from "../../components/MovieSpecs/movie-specs.component"
+
+const MovieDetails = () => { 
+    return (
+     <MovieSpecs/>
+    )
+}
+
+export default MovieDetails
