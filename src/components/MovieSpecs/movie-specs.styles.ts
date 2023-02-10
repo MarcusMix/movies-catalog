fe1a5253490c44359dcf23099c8afad527768f73
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-10 14:49:54 -0300

    feat: add movies specs styles and add in component

---
+++ src/components/MovieSpecs/movie-specs.styles.ts
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    margin: 0 20%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    & a {
+        text-decoration: none;
+    }
+`
+
+export const MovieDisplay = styled.div`
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: flex-start;
+    margin: 0 30%;
+
+    & h1 {
+        margin: auto;
+        padding: 1rem;
+        text-align: center;
+    }
+
+    & h3 {
+        text-align: flex-start;
+        padding: 0;
+        margin: 5px;
+    }
+
+    & img {
+        width: 250px;
+        height: 100%;
+        margin: auto;
+    }
+
+    & svg {
+        vertical-align: -10%;
+    }
+`
