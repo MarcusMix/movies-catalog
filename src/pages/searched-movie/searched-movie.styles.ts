46deadb8557e65b5e7d00ad7bc57c4cf20ac3b75
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-11 16:22:00 -0300

    feat: add searched movie styles and media querie

---
+++ src/pages/searched-movie/searched-movie.styles.ts
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+//router
+import { Link } from "react-router-dom";
+
+//splider
+import { SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/splide/dist/css/splide.min.css'
+
+
+export const Wrapper = styled.div`
+    margin: 0 10%;
+    & a {
+        text-decoration: none;
+    }
+`
+
+export const Card = styled(SplideSlide)`
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: flex-start;
+  
+
+    & h2 {
+        margin: auto;
+        padding: 1rem;
+        text-align: center;
+        font-size: 10px;
+    }
+
+    & img {
+        width: 400px;
+        height: 600px;
+        margin: auto;
+    }
+
+    @media (max-width: 768px) {
+
+        img {
+            width: 100%;
+            height: 100%;
+        }
+
+        & h2 {
+        margin: auto;
+        padding: 1rem;
+        text-align: center;
+        font-size: 15px;
+        }
+    }
+ 
+`
+
+export const SLink = styled(Link) `
+  text-decoration: none;
+  color: #313131;
+`
