0d89b19f208f730f33f570fa9f88e3a7477a6b8e
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-11 16:18:40 -0300

    refactor: change background button

--- src/components/Button/button.styles.ts
+++ src/components/Button/button.styles.ts
@@ -4,7 +4,7 @@ export const ButtonClick = styled.button`
   margin: 10px 10px;
   height: 1.8rem;
   border: none;
-  background-color: ${props => props.draggable ? '#FFC914' : 'white'};
+  background: ${props => props.draggable ? 'linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,199,18,1) 0%, rgba(255,255,6,1) 100%) ': 'white'};
   color: black;
   border-radius: 0.5rem;
   border: 2px solid #FFC914;
@@ -19,7 +19,7 @@ export const ButtonPages = styled(ButtonClick) `
   text-align: center;
 
   :active {
-    background-color: #FFC914;
+    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,199,18,1) 0%, rgba(255,255,6,1) 100%);
     box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
   }
 `
