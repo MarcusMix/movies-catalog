f252449ae22d958a7be59df99395a2adf5159f10
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-13 14:23:42 -0300

    feat: add theme provider and theme default and global styles

--- src/index.tsx
+++ src/index.tsx
@@ -17,38 +17,47 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 //pages
 import MovieDetails from './pages/movie-details/movie-details.page';
+import SearchedMovie from './pages/searched-movie/searched-movie.page';
 
 //components
 import Navbar from './components/Navbar/navbar.component';
 
 //styles
-import { NavbarContent, TitleYellow } from './components/Navbar/navbar.styles';
 import { SLink } from './components/Movies/movies.styles';
-import SearchedMovie from './pages/searched-movie/searched-movie.page';
+import { NavbarContent, TitleYellow } from './components/Navbar/navbar.styles';
+
+//themes
+import { ThemeProvider } from 'styled-components';
+import { lightTheme } from './theme/theme';
+import { GlobalStyles } from './theme/globalStyles';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <MovieProvider>
-        <Navbar> 
-        <NavbarContent>
-          <SLink to={'/'}>
-            <TitleYellow>
-              movies catalog
-            </TitleYellow>
-          </SLink>
-        </NavbarContent>
-        </Navbar>
-        <Routes>
-          <Route path='/' element={<App />}/>
-          <Route path='/movie-details/:name' element={<MovieDetails/>}/>
-          <Route path='/searched/:movie' element={<SearchedMovie/>}/>
-        </Routes>
-      </MovieProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyles/>
+        <BrowserRouter>
+          <MovieProvider>
+            <Navbar> 
+            <NavbarContent>
+              <SLink to={'/'}>
+                <TitleYellow>
+                  movies catalog
+                </TitleYellow>
+              </SLink>
+            </NavbarContent>
+            </Navbar>
+            <Routes>
+              <Route path='/' element={<App />}/>
+              <Route path='/movie-details/:name' element={<MovieDetails/>}/>
+              <Route path='/searched/:movie' element={<SearchedMovie/>}/>
+            </Routes>
+          </MovieProvider>
+        </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
