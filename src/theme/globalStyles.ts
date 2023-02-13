193ce9d75dd8478012f5774ae327be9b4b8dbc6a
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-13 14:21:38 -0300

    feat: add theme of project and new colors and bg

---
+++ src/theme/globalStyles.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+interface Props {
+    theme: {
+        body: string
+    }
+}
+
+export const GlobalStyles = createGlobalStyle<Props>`
+    body {
+        background-color: ${(props) => props.theme.body};
+    }
+`
---
+++ src/theme/theme.ts
@@ -0,0 +1,14 @@
+export const darkTheme = {
+  background: "#121212",
+  fontColor: '#fff',
+  border: '#FFC914',
+  green: '#09BC8A',
+  gradient: 'linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,199,18,1) 0%, rgba(255,255,6,1) 100%)'
+}
+
+//teste
+export const lightTheme = {
+  background: "#fff",
+  fontColor: '#121212',
+  border: '#FFC914'
+}
