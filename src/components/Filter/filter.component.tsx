1904b868b4fb8ed5935ced4929ec99b9ff9e11e2
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-15 10:39:50 -0300

    feat: add props whileTap in button filtering

--- src/components/Filter/filter.component.tsx
+++ src/components/Filter/filter.component.tsx
@@ -28,36 +28,42 @@ const Filter:FC<FilterProps> = ({ movies, setFiltered, setActiveGenre, activeGen
         <FilterContainer>
             <div>
             <ButtonClick
+                whileTap={{ scale: 0.9 }}
                 draggable={activeGenre === 0 ? true : false}
                 onClick={() => setActiveGenre(0)}>
                 Todos
             </ButtonClick>
 
             <ButtonClick 
+                whileTap={{ scale: 0.9 }}
                 draggable={activeGenre === 35 ? true : false}
                 onClick={() => setActiveGenre(35)}
                 >Comédia
             </ButtonClick>
 
             <ButtonClick 
+                whileTap={{ scale: 0.9 }}
                 draggable={activeGenre === 28 ? true : false}
                 onClick={() => setActiveGenre(28)}
                 >Ação
             </ButtonClick>
 
             <ButtonClick 
+                whileTap={{ scale: 0.9 }}
                 draggable={activeGenre === 12 ? true : false}
                 onClick={() => setActiveGenre(12)}
                 >Aventura
             </ButtonClick>
 
             <ButtonClick 
+                whileTap={{ scale: 0.9 }}
                 draggable={activeGenre === 27 ? true : false}
                 onClick={() => setActiveGenre(27)}
                 >Terror
             </ButtonClick>
 
             <ButtonClick 
+                whileTap={{ scale: 0.9 }}
                 draggable={activeGenre === 18 ? true : false}
                 onClick={() => setActiveGenre(18)}
                 >Drama
