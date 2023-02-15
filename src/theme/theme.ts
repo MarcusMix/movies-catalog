da01b34e4285916d5e7e3b5dba4f903f3966820e
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-15 10:37:00 -0300

    refactor: add gradient

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,15 +1,19 @@
-export const darkTheme = {
-  background: "#121212",
-  fontColor: '#fff',
+
+export const lightTheme = {
+  background: '#fff',
+  fontColor: '#121212',
   border: '#FFC914',
   green: '#09BC8A',
   red: '#a83f39',
-  gradient: 'linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,199,18,1) 0%, rgba(255,255,6,1) 100%)'
+  gradient: 'linear-gradient(to top, #ffc914, #f9d20c, #f3da07, #ebe309, #e3eb12)',
+  navbar: '#051923'
 }
-
-//teste
-export const lightTheme = {
-  background: "#fff",
-  fontColor: '#121212',
-  border: '#FFC914'
-}
+  export const darkTheme = {
+    background: '#121212',
+    fontColor: '#fff',
+    border: '#FFC914',
+    green: '#09BC8A',
+    red: '#a83f39',
+    gradient: 'linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,199,18,1) 0%, rgba(255,255,6,1) 100%)',
+    navbar: '#051923'
+  }
