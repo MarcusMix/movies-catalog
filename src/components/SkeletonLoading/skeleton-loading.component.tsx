a690129f43bd72107ba070e0cae422e404c1e47e
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-17 16:00:54 -0300

    feat: add skeleton component and styles

---
+++ src/components/SkeletonLoading/skeleton-loading.component.tsx
@@ -0,0 +1,30 @@
+
+import { RotatingSquare } from 'react-loader-spinner'
+import { RotateLoaderStyled, SkeletonStyled, SkeletonText, WrapperSkeleton } from './skeleton-loading.styles'
+
+const RotateLoading = () => {
+  return (
+    <RotateLoaderStyled>
+
+      <WrapperSkeleton>
+        <SkeletonText/>
+          <SkeletonStyled/> 
+      </WrapperSkeleton>
+
+      
+      <WrapperSkeleton>
+        <SkeletonText/>
+          <SkeletonStyled/> 
+      </WrapperSkeleton>
+
+      
+      <WrapperSkeleton>
+        <SkeletonText/>
+          <SkeletonStyled/> 
+      </WrapperSkeleton>
+      
+    </RotateLoaderStyled>
+  )
+}
+
+export default RotateLoading
