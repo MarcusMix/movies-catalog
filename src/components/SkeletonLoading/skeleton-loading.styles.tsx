858b4691290dfb9bb8f2633b0b559cd6280f345b
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-17 16:00:37 -0300

    feat: add keyframes animation and styles of skeleton loading

---
+++ src/components/SkeletonLoading/skeleton-loading.styles.tsx
@@ -0,0 +1,50 @@
+import styled, { keyframes } from "styled-components"
+
+const skeletonAnimation = keyframes`
+    0% {
+        background-color: #c0c3c5;
+    }
+    100% {
+        background-color: #f1f1f1;
+    }
+`
+
+export const RotateLoaderStyled = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+`
+
+export const WrapperSkeleton = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+    flex-direction: column;
+`
+
+export const SkeletonStyled = styled.div `
+    width: 400px;
+    height: 600px;
+    background-color: ${props => props.theme.lightGray};
+    margin: 1rem;
+    border-radius: 1rem;
+    animation: ${skeletonAnimation} 1s linear infinite alternate;
+    @media(max-width: 768px) {
+        width: 260px;
+        height: 400px;
+        overflow: hidden;
+        margin: 0 1rem;
+    }
+    `
+
+export const SkeletonText = styled.div`
+    background-color: ${props => props.theme.lightGray};
+    width: 70%;
+    height: 15px;
+    border-radius: 1rem;
+    margin: 2rem;
+    align-self: flex-start;
+    animation: ${skeletonAnimation} 1s linear infinite alternate;
+`
