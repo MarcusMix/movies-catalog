d1c0b0ff4da943b3fcf64bc812c6acb01a73c6ae
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-18 11:31:01 -0300

    fix: loading fixing

--- src/components/Movie-specs/movie-specs.component.tsx
+++ src/components/Movie-specs/movie-specs.component.tsx
@@ -30,6 +30,7 @@ const MovieSpecs = () => {
     const navigate = useNavigate()
 
     const [movieDetails, setMovieDetails] = useState<any>('')
+    const [loading, setLoading] = useState(true)
 
     const fetchMovies = async () => {
         const response = await fetch(
@@ -38,6 +39,7 @@ const MovieSpecs = () => {
         const data = await response.json()
         console.log(data)
         setMovieDetails(data)
+        setLoading(false)
     }
 
     useEffect(() => {
@@ -54,17 +56,23 @@ const MovieSpecs = () => {
             <MovieDisplay>
             <SkeletonTheme baseColor="#b9b9b9" highlightColor="#9e9e9e">
 
+            {loading ? <Skeleton count={1} style={{width: '300px', marginTop: '1rem'}} /> : (
+                <h1>{movieDetails.title}</h1> 
+            ) }
             
-            <h1>{movieDetails.title || <Skeleton/>}</h1> 
+            {loading ? <Skeleton height={300} count={1} style={{width: '500px', marginTop: '1rem'}} /> : (
+                <img src={imageURL + movieDetails.backdrop_path} alt={movieDetails.title}/> 
+               
+            )}
 
-            {<img src={imageURL + movieDetails.backdrop_path} alt={movieDetails.title} /> 
-            || 
-            <div><Skeleton style={{maxWidth: '600px', minWidth: '200px'}}/></div>}
-
-            {<h3> <BsCalendarCheckFill color='#09BC8A'/> Lançamento: {movieDetails.release_date}</h3> || <Skeleton/>}
-            {<h3> <BsFillHeartFill color='#CC0000'/> Nota: {movieDetails.vote_average}</h3> || <Skeleton/>}
-            {<h3> <BiTime /> Duração: {movieDetails.runtime} minutos</h3> || <Skeleton/>}
-            {<h3> <FaTheaterMasks color='#1B98E0'/> Gêneros</h3> || <Skeleton/> }
+            {loading ? <Skeleton count={1} style={{width: '300px', marginTop: '1rem'}} /> : (
+                <div>
+                    <h3> <BsCalendarCheckFill color='#09BC8A'/> Lançamento: {movieDetails.release_date}</h3>
+                    <h3> <BsFillHeartFill color='#CC0000'/> Nota: {movieDetails.vote_average}</h3>
+                    <h3> <BiTime /> Duração: {movieDetails.runtime} minutos</h3> 
+                    <h3> <FaTheaterMasks color='#1B98E0'/> Gêneros</h3>
+                </div>
+            )}
             <div>
                 <ul>
                     {movieDetails.genres?.map((type: any) => {
@@ -74,8 +82,10 @@ const MovieSpecs = () => {
                         })}
                 </ul>
             </div>
-            <h3> <RiFilePaper2Fill color='#F7CE5B'/> Sinópse</h3>
-            {<p>{movieDetails.overview}</p> || <Skeleton count={5}/>}
+            {loading ? <Skeleton count={8} style={{width: '500px', marginTop: '1rem'}} /> : (
+                <h3> <RiFilePaper2Fill color='#F7CE5B'/> Sinópse</h3>
+            )}
+            <p>{movieDetails.overview}</p> 
             </SkeletonTheme>
             </MovieDisplay>
         </Wrapper>
