011a509b1cf4638dd9989065fc043d307cb3f3aa
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-18 09:54:20 -0300

    feat: add image not found case image unavailable

--- src/components/Movies/movies.component.tsx
+++ src/components/Movies/movies.component.tsx
@@ -7,6 +7,9 @@ import { motion } from 'framer-motion'
 //interface
 import MovieProps from "../../types/movies.types"
 
+//image
+import ImageNotFoundPng from '../../assets/imageNotFound.jpg'
+
 const Movie:FC<MovieProps>  = ({ movie })=> {
     return (
         <motion.div 
@@ -17,10 +20,14 @@ const Movie:FC<MovieProps>  = ({ movie })=> {
             whileHover={{ scale: 1.05 }}
         > 
             <h2>{movie.title}</h2>
-            <img 
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
-                alt={movie.title} 
-            />
+            {movie.poster_path ? (
+                <img 
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+                    alt={movie.title} 
+                />
+            ) : (
+                <img src={ImageNotFoundPng} alt="Imagem não encontrada!"/>
+            )}
         </motion.div>
     )
 }
