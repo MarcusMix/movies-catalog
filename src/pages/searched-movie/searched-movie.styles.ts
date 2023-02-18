c9ac44c12f1cda515663b2f9a0e311158d932f02
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-18 09:49:47 -0300

    style: add somes styles, margin, padding

--- src/pages/searched-movie/searched-movie.styles.ts
+++ src/pages/searched-movie/searched-movie.styles.ts
@@ -12,6 +12,11 @@ export const Wrapper = styled.div`
     & a {
         text-decoration: none;
     }
+
+    @media (max-width: 768px) {
+        /* margin: 0 5%; */
+    }
+
 `
 
 export const Card = styled(SplideSlide)`
@@ -32,6 +37,8 @@ export const Card = styled(SplideSlide)`
         width: 400px;
         height: 600px;
         margin: auto;
+        padding: 0;
+        box-shadow: rgba(17, 17, 26, 0.05) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px;
     }
 
     @media (max-width: 768px) {
@@ -60,4 +67,44 @@ export const WrapperInside = styled(Wrapper)`
     justify-content: center;
     align-items: center;
     margin: 3rem 0;
+`
+
+//delirio kkkk
+export const ImageNotFound = styled.div`
+    width: 400px;
+    height: 590px;
+    margin: auto;
+    background-color: ${props => props.theme.border};
+    border-radius: 1rem;
+    margin-top: 5px;
+    box-shadow: rgba(17, 17, 26, 0.05) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    & h2 {
+        color: ${props => props.theme.red};
+        align-self: center;
+        margin-left: 20%;
+    }
+
+    & svg {
+        width: 300px;
+    }
+
+    @media (max-width: 768px) {
+         width: 100%;
+         height: 100%;
+         margin: auto;
+         padding: 0;
+         margin-right: 0.3rem;
+
+        & h2 {
+            margin: auto;
+            padding: 1rem;
+            text-align: center;
+            font-size: 15px;
+        }        
+    }
 `
