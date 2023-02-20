a1ea1088539670659700439c24b431b21124acdb
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-20 14:51:20 -0300

    refactor: change import of image error

--- src/components/Movies/movies.component.tsx
+++ src/components/Movies/movies.component.tsx
@@ -8,7 +8,7 @@ import { motion } from 'framer-motion'
 import MovieProps from "../../types/movies.types"
 
 //image
-import ImageNotFoundPng from '../../assets/imageNotFound.jpg'
+import ImageNotFoundPng from '../../assets/image-error-short.png'
 
 const Movie:FC<MovieProps>  = ({ movie })=> {
     return (
