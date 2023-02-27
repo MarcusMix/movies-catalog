18554bcac0da9d4725c47fbd34d3e0dc31c0c1a6
Marcus Sandi <tradedomarcus@gmail.com>
2023-02-27 08:53:00 -0300

    refactor: add string type to color and add ?  for children and onClick

--- src/types/button.types.ts
+++ src/types/button.types.ts
@@ -1,9 +1,9 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
-    children: ReactNode
-    onClick: (e?: any) => void | any;
-    color?: any 
+    children?: ReactNode
+    onClick?: (e?: any) => void | any;
+    color?: string 
     whileTap?: any
     whileHover?: any
 }
