5349715d0c428f49dfa0474776298be2833fbb8a
Marcus Sandi <tradedomarcus@gmail.com>
2023-03-02 13:32:16 -0300

    feat: add IMoviesSpecs with props of movies

---
+++ src/types/movies-specs.types.ts
@@ -0,0 +1,12 @@
+interface IMoviesSpecs {
+    id: number
+    title: string
+    backdrop_path: string
+    release_date: string
+    vote_average: number
+    runtime: number
+    genres: string[]
+    overview: string
+}
+
+export default IMoviesSpecs
