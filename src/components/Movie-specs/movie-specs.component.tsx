2268b5c5fbf9fe443e58dce23931495b823d370b
Marcus Sandi <tradedomarcus@gmail.com>
2023-03-02 13:32:53 -0300

    feat: add interface in useState movieDetails

--- src/components/Movie-specs/movie-specs.component.tsx
+++ src/components/Movie-specs/movie-specs.component.tsx
@@ -27,6 +27,9 @@ import moment from 'moment'
 //image
 import imageNotFound from '../../assets/image-error-wide.png'
 
+//interfaces & types
+import IMoviesSpecs from '../../types/movies-specs.types'
+
 const MovieSpecs = () => {
 
     const imageURL = 'https://image.tmdb.org/t/p/w500'
@@ -35,7 +38,7 @@ const MovieSpecs = () => {
 
     const navigate = useNavigate()
 
-    const [movieDetails, setMovieDetails] = useState<any>('')
+    const [movieDetails, setMovieDetails] = useState<IMoviesSpecs | undefined>(undefined)
     const [loading, setLoading] = useState(true)
 
     const fetchMovies = async () => {
@@ -57,7 +60,7 @@ const MovieSpecs = () => {
         window.scroll({
             top: 0,
             behavior: 'smooth'
-          })
+        })
     }
 
     moment.locale();
@@ -69,32 +72,27 @@ const MovieSpecs = () => {
                 <SkeletonTheme baseColor="#b9b9b9" highlightColor="#9e9e9e">
 
                 {loading ? <Skeleton count={1} style={{width: '320px', margin: '1rem'}} /> : (
-                    <h1>{movieDetails.title}</h1> 
+                    <h1>{movieDetails?.title}</h1> 
                 )}
         
-                {movieDetails.backdrop_path ? <img src={imageURL + movieDetails.backdrop_path} alt={movieDetails.title}/> : (
-                    <img src={imageNotFound} alt={movieDetails.title}/>
+                {movieDetails?.backdrop_path ? <img src={imageURL + movieDetails?.backdrop_path} alt={movieDetails?.title}/> : (
+                    <img src={imageNotFound} alt={movieDetails?.title}/>
                 )}
-
-                {/* {loading ? <Skeleton height={300} count={1} style={{width: '350px', margin: '1rem'}} /> : (
-                    <img src={imageURL + movieDetails.backdrop_path} alt={movieDetails.title}/> 
-                
-                )} */}
             
                 {loading ? <Skeleton count={1} style={{width: '320px', marginLeft: '1rem'}} /> : (
                     <div>
-                        <h3> <BsCalendarCheckFill color='#09BC8A'/> Lançamento: {moment(movieDetails.release_date).format('L')}</h3>
-                        <h3> <BsFillHeartFill color='#CC0000'/> Nota: {movieDetails.vote_average === 0 ? ' não encontrada!' : movieDetails.vote_average} </h3>
+                        <h3> <BsCalendarCheckFill color='#09BC8A'/> Lançamento: {moment(movieDetails?.release_date).format('L')}</h3>
+                        <h3> <BsFillHeartFill color='#CC0000'/> Nota: {movieDetails?.vote_average === 0 ? ' não encontrada!' : movieDetails?.vote_average} </h3>
                     
-                        <h3> <BiTime /> Duração: {movieDetails.runtime} minutos</h3> 
+                        <h3> <BiTime /> Duração: {movieDetails?.runtime} minutos</h3> 
                         <h3> <FaTheaterMasks color='#1B98E0'/> Gêneros</h3>
                     </div>
                 )}
                 <div>
                     <ul>
-                        {movieDetails.genres?.map((type: any) => {
+                        {movieDetails?.genres?.map((type: any) => {
                             return (
-                                <li key={movieDetails.id}>{type.name}</li>
+                                <li key={movieDetails?.id}>{type.name}</li>
                                 )
                             })}
                     </ul>
@@ -102,7 +100,7 @@ const MovieSpecs = () => {
                 {loading ? <Skeleton count={8} style={{width: '320px', margin: '1rem'}} /> : (
                     <h3> <RiFilePaper2Fill color='#F7CE5B'/> Sinópse</h3>
                 )}
-                <p>{movieDetails.overview === "" ? "Sinópse não encontrada!" : movieDetails.overview}</p> 
+                <p>{movieDetails?.overview === "" ? "Sinópse não encontrada!" : movieDetails?.overview}</p> 
                 </SkeletonTheme>
                 </MovieDisplay>
             </Wrapper>
