f7439eb5b6bdec10422eb968613dae20dae0b0f9
Marcus Sandi <tradedomarcus@gmail.com>
2023-04-02 12:19:27 -0300

    feat: movie not found component (when movie not exists)

---
+++ src/components/MovieNotFound/movie-not-found.tsx
@@ -0,0 +1,13 @@
+import { SectionMovie, SectionMovieItem } from './movie-not-foun';
+
+const MovieNotFound = () => {
+  return (
+    <SectionMovie>
+      <SectionMovieItem>
+        <h1>Filme não encontrado!</h1>
+      </SectionMovieItem>
+    </SectionMovie>
+  );
+};
+
+export default MovieNotFound;
